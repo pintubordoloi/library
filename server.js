d52044f1aaa2ae1b23922b3a3348c470a2d811f3
pintubordoloi <41015722+pintubordoloi@users.noreply.github.com>
2021-04-09 21:13:32 +0530

    Search adding

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 
 dotenv.config({path:'env'})
 
@@ -10,12 +11,14 @@ DATABASE_URL = 'mongodb+srv://dango:dango123@cluster0.0lkqu.mongodb.net/myFirstD
 
 //requiring all the routes
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //creating connection with the mongodb
 mongoose.connect(DATABASE_URL,{
@@ -27,6 +30,7 @@ db.once('open', () => console.log('Connected to mongoose'));
 
 //using all the routes
 app.use('/', indexRouter);
+app.use('/authors',authorRouter);
 
 
 //listening to port 3000
